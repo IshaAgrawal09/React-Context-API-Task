caf4dc2d9f8ab19795a305f2765d3611d689212b
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-08-17 10:43:39 +0530

    Currency Convertor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  const fetchRes = () => {
+    let data = document.getElementById("inp").value;
+    let source = document.getElementById("from").value;
+    let destination = document.getElementById("to").value;
+    let res = data;
+
+    if (destination === "INR" && source === "USD") {
+      res = (Number(data) * 73.70426).toFixed(2);
+    }
+    if (destination === "USD" && source === "INR") {
+      res = (Number(data) / 73.70426).toFixed(2);
+    }
+
+    let str = `${data} ${source} = ${res} ${destination}`;
+    setMsg(str);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={3}>Currency Convertor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan={3}>Enter Amount</td>
+          </tr>
+          <tr>
+            <td colSpan={3}>
+              <input type="number" id="inp"></input>
+            </td>
+          </tr>
+          <tr>
+            <td>From</td>
+            <td></td>
+            <td>To </td>
+          </tr>
+          <tr>
+            <td>
+              <select id="from">
+                <option value="USD">USD </option>
+                <option value="INR">INR </option>
+              </select>
+            </td>
+            <td>
+              <i class="fa-solid fa-arrow-right-arrow-left"></i>
+            </td>
+            <td>
+              <select id="to">
+                <option value="INR">INR </option>
+                <option value="USD">USD </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={3} id="res">
+              {msg}
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={3} id="res">
+              <button onClick={fetchRes}>Get Exchange Rate</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
