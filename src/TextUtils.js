cbbf87fadcdcd3666e2d9f2770525d84f8f593c3
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-08-30 18:31:27 +0530

    Updated text utility

--- src/TextUtils.js
+++ src/TextUtils.js
@@ -1,34 +1,50 @@
-import React,{ useState } from 'react'
-
-
+import React, { useState } from "react";
 
 const TextUtils = () => {
-    const [text,setText] = useState("")
-    const [word,setWord] = useState(1)
-   
-    // setWord(
-    //     text.split().length
-    // )
+  const [text, setText] = useState("");
+  const [word, setWord] = useState(0);
+  const [char, setChar] = useState(0);
+  const [display, setDisplay] = useState("none");
 
-    const handleInput = (event) =>{
-        setText(event.target.value)
+  const handleInput = (event) => {
+    setText(event.target.value);
+  };
+  const upperCase = () => {
+    setText(text.toUpperCase());
+  };
+  const lowerCase = () => {
+    setText(text.toLowerCase());
+  };
+  const wordShow = () => {
+    if (text.split(" ").length == 1) {
+      setWord(0);
+    } else {
+      setWord(text.split(" ").length);
     }
-    const upperCase = () =>{
-        setText(text.toUpperCase())
-    }
-    const lowerCase = ()=>{
-        setText(text.toLowerCase())
-    }
+    setChar(text.length);
+    setDisplay("block");
+  };
   return (
-    <div className='textUtils'>
-        <textarea type='text' id='text' placeholder='Enter Text' value={text} onChange={handleInput} rows={4} cols={50}></textarea>
-        <br />
-        <button onClick={upperCase}>UPPER CASE</button>
-        <button onClick={lowerCase}>LOWER CASE</button>
-        <h4>Summary</h4>
-        <p>{text.split(" ").length} Words and {text.length} Characters </p>
-    </div>
-  )
-}
+    <div className="textUtils">
+      <textarea
+        type="text"
+        id="text"
+        placeholder="Enter Text"
+        value={text}
+        onChange={handleInput}
+        rows={4}
+        cols={50}
+      ></textarea>
+      <br />
+      <button onClick={upperCase}>UPPER CASE</button>
+      <button onClick={lowerCase}>LOWER CASE</button>
+      <button onClick={wordShow}> Show Summary </button>
 
-export default TextUtils
+      <p style={{ display: display }}>
+        {word} Words and {char} Characters
+      </p>
+    </div>
+  );
+};
+
+export default TextUtils;
